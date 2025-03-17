93ca7323634a4fa8fcade4b40ce86f88e6700a3a
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-17 16:01:08 -0700

    Create methods and Javadocs

--- core/src/main/java/io/github/ZombieSurvival/Entity.java
+++ core/src/main/java/io/github/ZombieSurvival/Entity.java
@@ -1,13 +1,60 @@
 package io.github.ZombieSurvival;
 
+/**
+ * A non-player entity.
+ *
+ * @author Kanon Nishiyama
+ * @version 2025
+ */
 public abstract class Entity {
-    protected int value;
+    /**
+     * Value the entity will modify the Player's stats by.
+     */
+    protected final int value;
+    /**
+     * Entity's X coordinate.
+     */
     protected int xCoordinate;
+    /**
+     * Entity's Y coordinate.
+     */
     protected int yCoordinate;
 
+    /**
+     * Constructs an Entity object with the specified value, xCoordinate, and yCoordinate.
+     *
+     * @param value an int
+     * @param xCoordinate an int
+     * @param yCoordinate an int
+     */
     public Entity(final int value, final int xCoordinate, final int yCoordinate) {
         this.value = value;
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
+    }
+
+    /**
+     * Modifies the given Player's stats by value.
+     *
+     * @param player a Player
+     */
+    void modifyPlayerStat(final Player player) {
+        System.out.print("Modifying player stat by value!");
+    }
+    /**
+     * Returns the xCoordinate of this Entity.
+     *
+     * @return xCoordinate as int
+     */
+    int getXCoordinate() {
+        return xCoordinate;
+    }
+    /**
+     * Returns the yCoordinate of this Entity.
+     *
+     * @return yCoordinate as int
+     */
+    int getYCoordinate() {
+        return yCoordinate;
     }
 }
