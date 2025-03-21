cc8ede39a04bb48fa1d517b3e87fe8d6880af8ad
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-21 09:14:50 -0700

    Create ItemType enum

---
+++ core/src/main/java/io/github/ZombieSurvival/ItemType.java
@@ -0,0 +1,13 @@
+package io.github.ZombieSurvival;
+
+/**
+ * Item type representing the affected player status.
+ *
+ * @author Kanon Nishiyama
+ * @version 2025
+ */
+public enum ItemType {
+    HP,
+    STAMINA,
+    SCORE
+}
