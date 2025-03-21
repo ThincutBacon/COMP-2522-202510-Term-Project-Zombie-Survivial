e71173b17ff60d01da0e63f8b71aae41976c43ed
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-21 09:18:46 -0700

    Create overridden equals, hashCode, and toString methods

--- core/src/main/java/io/github/ZombieSurvival/Entity.java
+++ core/src/main/java/io/github/ZombieSurvival/Entity.java
@@ -38,15 +38,21 @@ public abstract class Entity {
      *
      * @param player a Player
      */
-    void modifyPlayerStat(final Player player) {
-        System.out.print("Modifying player stat by value!");
+    abstract void modifyPlayerStat(Player player);
+    /**
+     * Returns the value of this Entity.
+     *
+     * @return value as int
+     */
+    protected int getValue() {
+        return value;
     }
     /**
      * Returns the xCoordinate of this Entity.
      *
      * @return xCoordinate as int
      */
-    int getXCoordinate() {
+    public int getXCoordinate() {
         return xCoordinate;
     }
     /**
@@ -54,7 +60,61 @@ public abstract class Entity {
      *
      * @return yCoordinate as int
      */
-    int getYCoordinate() {
+    public int getYCoordinate() {
         return yCoordinate;
+    }
+
+    /**
+     * Compares this Entity to another Object for equality.
+     *
+     * @param object an Object
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        Entity entity = (Entity) object;
+
+        return getValue() == entity.getValue()
+            && getXCoordinate() == entity.getXCoordinate()
+            && getYCoordinate() == entity.getYCoordinate();
+    }
+
+    /**
+     * Returns a hash code for this Entity.
+     *
+     * @return a hash code
+     */
+    @Override
+    public int hashCode() {
+        final int usefulPrime = 23;
+        int result;
+
+        result = getValue();
+        result = usefulPrime * result + getXCoordinate();
+        result = usefulPrime * result + getYCoordinate();
+        return result;
+    }
+
+    /**
+     * Returns a String representation of this Loan.
+     *
+     * @return a String
+     */
+    @Override
+    public String toString() {
+        final StringBuilder builder;
+        builder = new StringBuilder("Entity{\n");
+        builder.append("value='").append(getValue()).append("', \n");
+        builder.append("xCoordinate=").append(getXCoordinate()).append(", \n");
+        builder.append("yCoordinate='").append(getYCoordinate());
+        builder.append("\n}");
+        return builder.toString();
     }
 }
