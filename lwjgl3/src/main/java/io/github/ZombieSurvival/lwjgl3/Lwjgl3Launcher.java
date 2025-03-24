ff959d824c42e7e97db7c73a5ea55f9d783c8e73
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-24 13:58:29 -0700

    Adjust to run RotNRun class instead of Main

--- lwjgl3/src/main/java/io/github/ZombieSurvival/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/ZombieSurvival/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package io.github.ZombieSurvival.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.ZombieSurvival.Main;
+import io.github.ZombieSurvival.RotNRun;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return new Lwjgl3Application(new RotNRun(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
