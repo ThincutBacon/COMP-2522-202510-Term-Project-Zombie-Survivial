be82749a363132c89fa70b4da38b25651f0b7ec0
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-24 14:01:25 -0700

    Create clickable start button on main menu

--- core/src/main/java/io/github/ZombieSurvival/MainMenuScreen.java
+++ core/src/main/java/io/github/ZombieSurvival/MainMenuScreen.java
@@ -2,13 +2,38 @@ package io.github.ZombieSurvival;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
+/**
+ * The main menu of the game.
+ *
+ * @author Kanon Nishiyama
+ * @version 2025
+ */
 public class MainMenuScreen implements Screen {
     private final RotNRun game;
 
+    private final Texture startButtonInactive;
+    private final Texture startButtonActive;
+
+    /**
+     * Constructs a MainMenuScreen object with the specified instance of game.
+     *
+     * @param game a RotNRun
+     * @throws IllegalArgumentException if game is null
+     */
     public MainMenuScreen(final RotNRun game) {
+        if (game == null) {
+            throw new IllegalArgumentException("There is no game to the apply screen to.");
+        }
         this.game = game;
+
+        startButtonInactive = new Texture("Start_Button_Inactive.png");
+        startButtonActive = new Texture("Start_Button_Active.png");
     }
 
     @Override
@@ -16,33 +41,67 @@ public class MainMenuScreen implements Screen {
 
     }
 
+    private float getPositionOffsetX(final float frameWidth, final BitmapFont bitmapFont, final String value) {
+        GlyphLayout glyphLayout = new GlyphLayout();
+        glyphLayout.setText(bitmapFont, value);
+        return (frameWidth / 2) - (glyphLayout.width / 2);
+    }
+
+    private float getPositionOffsetY(final float frameHeight, final BitmapFont bitmapFont, final String value) {
+        GlyphLayout glyphLayout = new GlyphLayout();
+        glyphLayout.setText(bitmapFont, value);
+        return (frameHeight / 2) - (glyphLayout.height / 2);
+    }
+
+    /**
+     * Renders this MainMenuScreen.
+     *
+     * @param delta a float
+     */
     @Override
     public void render(final float delta) {
-        // Background color/cleared screen color
-        final float red = 27f / 255f;
-        final float blue = 14f / 255f;
-        final float green = 25f / 255f;
-        ScreenUtils.clear(red, blue, green, 1, true);
+        // Clears screen
+        game.applyViewport();
 
-        game.getViewport().apply();
-        game.getSpriteBatch().setProjectionMatrix(game.getViewport().getCamera().combined);
-
-        game.getSpriteBatch().begin();
+        // Draws elements to screen
+        SpriteBatch batch = game.getSpriteBatch();
+        BitmapFont font = game.getFont();
+        Viewport viewport = game.getViewport();
+        float xCenter = getPositionOffsetX(game.getViewport().getWorldWidth(), font, "Welcome to Rot 'N' Run!!!");
+        float yCenter = getPositionOffsetY(game.getViewport().getWorldHeight(), font, "Welcome to Rot 'N' Run!!!");
+        float x2Center = getPositionOffsetX(game.getViewport().getWorldWidth(), font, "Tap anywhere to begin!");
+        batch.begin();
             //draw text. Remember that x and y are in meters
-            game.getFont().draw(game.getSpriteBatch(), "Welcome to Rot 'N' Run!!! ", 1, 1.5f);
-            game.getFont().draw(game.getSpriteBatch(), "Tap anywhere to begin!", 1, 1);
-        game.getSpriteBatch().end();
+            font.draw(batch, "Welcome to Rot 'N' Run!!!", xCenter, yCenter + 300f);
+            font.draw(batch, "Tap anywhere to begin!", x2Center, yCenter + 220);
+
+            game.setMousePosition();
+
+            if ((RotNRun.MOUSE_POSITION.x >= viewport.getWorldWidth() / 2 - 450 && RotNRun.MOUSE_POSITION.x <= viewport.getWorldWidth() / 2 + 450)
+            && (RotNRun.MOUSE_POSITION.y >= yCenter - 80 && RotNRun.MOUSE_POSITION.y <= yCenter - 80 + 90)) {
+                batch.draw(startButtonActive, viewport.getWorldWidth() / 2 - 450, yCenter - 80, 900, 90);
+            } else {
+                batch.draw(startButtonInactive, viewport.getWorldWidth() / 2 - 450, yCenter - 80, 900, 90);
+            }
+
+
+
+        batch.end();
 
         // Game start on touch
         if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game));
-            dispose();
+            game.setMousePosition();
+            if ((RotNRun.MOUSE_POSITION.x >= RotNRun.VIRTUAL_WIDTH / 2 - 450 && RotNRun.MOUSE_POSITION.x <= RotNRun.VIRTUAL_WIDTH / 2 + 450)
+                && (RotNRun.MOUSE_POSITION.y >= yCenter - 80 && RotNRun.MOUSE_POSITION.y <= yCenter - 80 + 90)) {
+                game.setScreen(new GameScreen(game));
+                dispose();
+            }
         }
     }
 
     @Override
     public void resize(final int width, final int height) {
-        game.getViewport().update(width, height, true);
+        game.updateViewport(width, height);
     }
 
     @Override
