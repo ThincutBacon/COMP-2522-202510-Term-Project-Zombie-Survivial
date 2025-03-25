486f30e7003358cf0ab10626388536e5cc476406
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-25 15:05:53 -0700

    Initiate variables

--- core/src/main/java/io/github/ZombieSurvival/Screens/GameScreen.java
+++ core/src/main/java/io/github/ZombieSurvival/Screens/GameScreen.java
@@ -3,21 +3,16 @@ package io.github.ZombieSurvival.Screens;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.graphics.Color;
-import io.github.ZombieSurvival.Sprites.Generate;
-import io.github.ZombieSurvival.RotNRun;
-import io.github.ZombieSurvival.Sprites.Difficulty;
+import com.badlogic.gdx.utils.TimeUtils;
 import io.github.ZombieSurvival.Sprites.Player;
-
-import java.awt.*;
+import io.github.ZombieSurvival.Sprites.Enemy;
+import io.github.ZombieSurvival.Sprites.Item;
+import io.github.ZombieSurvival.Sprites.Generate;
+import io.github.ZombieSurvival.Sprites.Difficulty;
+import io.github.ZombieSurvival.RotNRun;
 
 /**
  * The game screen of the game.
@@ -28,72 +23,52 @@ import java.awt.*;
 public class GameScreen implements Screen {
     // Current running game
     private final RotNRun game;
+    // Sprite size
+    private final float spriteWidth = 80f;
+    private final float spriteHeight = 1.5f * spriteWidth;
+
     // Background
     private Texture backgroundTexture;
     private Texture platformTexture;
     // Player
-    private Player playerStats;
     private Texture playerTexture;
-    private Sprite playerSprite;
+    private Player playerSprite;
     private Rectangle playerHitbox;
     // Enemy
     private Texture standardZombieTexture;
-    private Array<Sprite> enemySprites;
+    private Array<Enemy> enemySprites;
     private Rectangle enemyHitbox;
-    // HUD
-    private Stage HUD;
-    private Table layout;
-    private Label scoreLabel;
-    private Label hpLabel;
-    private Label nameLabel;
+    // Item
+    private Array<Item> itemSprites;
+    private Rectangle itemHitbox;
     // Backend
-    private Vector2 touchPosition;
     private float enemySpawnTimer;
     private float itemSpawnTimer;
     private long startGameInMilliSeconds;
-    private long endGameInMilliSeconds;
-    // Numbers
-    private float worldWidth;
-    private float worldHeight;
-    private float worldXCenter;
-    private float worldYCenter;
-
-    private final float spriteWidth = 1f;
-    private final float spriteHeight = 1.5f;
-
 
     public GameScreen(final RotNRun game) {
         this.game = game;
-        worldWidth = game.getViewport().getWorldWidth();
-        worldHeight = game.getViewport().getWorldHeight();
-        worldXCenter = worldWidth / 2;
-        worldYCenter = worldHeight / 2;
-
-        playerStats = Generate.createPlayer(playerTexture, Difficulty.NORMAL);
-        // Load images
+        // Load textures
+        // Background
         backgroundTexture = new Texture("City_Ruins.png");
+        platformTexture = new Texture("Map_Platform.png");
+        // Player
         playerTexture = new Texture("Player_Sprite_Large.png");
-        standardZombieTexture = new Texture("Zombie_Sprite_Large.png");
-
-        playerSprite = new Sprite(playerTexture);
+        playerHitbox = new Rectangle();
+        playerSprite = Generate.createPlayer(playerTexture, Difficulty.NORMAL);
         playerSprite.setSize(spriteWidth, spriteHeight);
-
-        playerSprite.setCenter(worldXCenter, worldYCenter);
-
-        HUD = new Stage(game.getViewport(), game.getSpriteBatch());
-        layout = new Table();
-        layout.top();
-        layout.setFillParent(true);
-
-        scoreLabel = new Label("100", new Label.LabelStyle(game.getFont(), Color.WHITE));
-        hpLabel = new Label("3", new Label.LabelStyle(game.getFont(), Color.WHITE));
-        nameLabel = new Label("THIS IS A GAME", new Label.LabelStyle(game.getFont(), Color.WHITE));
-
-        layout.add(scoreLabel).expandX().padTop(10);
-        layout.add(nameLabel).expandX().padTop(10);
-        layout.add(hpLabel).expandX().padTop(10);
-
-        HUD.addActor(layout);
+        playerSprite.setCenter((RotNRun.VIRTUAL_WIDTH / 2f), (RotNRun.VIRTUAL_HEIGHT / 2f));
+        // Enemies
+        standardZombieTexture = new Texture("Zombie_Sprite_Large.png");
+        enemySprites = new Array<>();
+        enemyHitbox = new Rectangle();
+        // Items
+        itemSprites = new Array<>();
+        itemHitbox = new Rectangle();
+        // Backend
+        enemySpawnTimer = 0;
+        itemSpawnTimer = 0;
+        startGameInMilliSeconds = TimeUtils.millis();
     }
 
     @Override
@@ -101,48 +76,40 @@ public class GameScreen implements Screen {
 
     }
 
-    private float getPositionOffsetX(final float frameWidth, final BitmapFont bitmapFont, final String value) {
-        GlyphLayout glyphLayout = new GlyphLayout();
-        glyphLayout.setText(bitmapFont, value);
-        return (frameWidth / 2) - (glyphLayout.width / 2);
-    }
-
-    private float getPositionOffsetY(final float frameHeight, final BitmapFont bitmapFont, final String value) {
-        GlyphLayout glyphLayout = new GlyphLayout();
-        glyphLayout.setText(bitmapFont, value);
-        return (frameHeight / 2) - (glyphLayout.height / 2);
-    }
-
     @Override
     public void render(final float delta) {
         // Clears screen
         game.applyViewport();
-        game.getSpriteBatch().setProjectionMatrix(HUD.getCamera().combined);
 
-        // Draws elements to screen
+        // For brevity
         SpriteBatch batch = game.getSpriteBatch();
         BitmapFont font = game.getFont();
-        float worldWidth = game.getViewport().getWorldWidth();
-        float worldHeight = game.getViewport().getWorldHeight();
+        // Draw elements to screen
+        draw(batch, font);
+    }
+
+    private void draw(final SpriteBatch batch, final BitmapFont font) {
+        // Platform size
+        final float platformWidth = 9f * 150f;
+        final float platformHeight = 7f * 150f;
+        // Draws elements to screen
         batch.begin();
-            //draw text. Remember that x and y are in meters
-            batch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+            batch.draw(backgroundTexture, 0, 0, RotNRun.VIRTUAL_WIDTH, RotNRun.VIRTUAL_HEIGHT);
+            batch.draw(platformTexture, RotNRun.VIRTUAL_WIDTH / 2f - platformWidth / 2f,
+                0, platformWidth, platformHeight);
             playerSprite.draw(batch);
+            for (Item item : itemSprites) {
+                item.draw(batch);
+            }
+            for (Enemy enemy : enemySprites) {
+                enemy.draw(batch);
+            }
         batch.end();
-
-
-        HUD.draw();
-        playerSprite.setCenter(worldXCenter, 1);
-        playerSprite.setCenter(worldXCenter, 2);
     }
 
     @Override
     public void resize(final int width, final int height) {
         game.updateViewport(width, height);
-        worldWidth = game.getViewport().getWorldWidth();
-        worldHeight = game.getViewport().getWorldHeight();
-        worldXCenter = worldWidth / 2;
-        worldYCenter = worldHeight / 2;
     }
 
     @Override
