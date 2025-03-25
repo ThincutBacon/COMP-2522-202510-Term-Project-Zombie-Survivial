e5dddd31e79a479b235df017e28d3156a371813e
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-25 16:17:36 -0700

    Create methods to generate enemies over time

--- core/src/main/java/io/github/ZombieSurvival/Screens/GameScreen.java
+++ core/src/main/java/io/github/ZombieSurvival/Screens/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -23,11 +24,23 @@ import io.github.ZombieSurvival.RotNRun;
  * @version 2025
  */
 public class GameScreen implements Screen {
-    // Sprite size
+    // Sprite
     private static final float SPRITE_WIDTH = 80f;
     private static final float SPRITE_HEIGHT = 1.5f * SPRITE_WIDTH;
+    // Platform
+    private static final float PLATFORM_WIDTH = 9f * 150f;
+    private static final float PLATFORM_HEIGHT = 7f * 150f;
+    private static final float PLATFORM_X = (RotNRun.VIRTUAL_WIDTH / 2f) - (PLATFORM_WIDTH / 2f);
+    // Platform play area
+    private static final float PLATFORM_AREA_PADDING = 20f;
+    private static final float PLATFORM_AREA_X = PLATFORM_X + PLATFORM_AREA_PADDING;
+    private static final float PLATFORM_AREA_MAX_X = PLATFORM_X + PLATFORM_WIDTH
+                                                - SPRITE_WIDTH - PLATFORM_AREA_PADDING;
+    private static final float PLATFORM_AREA_Y = 150f + PLATFORM_AREA_PADDING;
+    private static final float PLATFORM_AREA_MAX_Y = PLATFORM_HEIGHT - SPRITE_HEIGHT
+                                                + PLATFORM_AREA_PADDING;
     // Player movement speed
-    private static final float PLAYER_SPEED = 20f;
+    private static final float PLAYER_SPEED = 300f;
 
     // Current running game
     private final RotNRun game;
@@ -90,35 +103,73 @@ public class GameScreen implements Screen {
         // For brevity
         SpriteBatch batch = game.getSpriteBatch();
         BitmapFont font = game.getFont();
+        // Increments and checks on timers
+        checkTimers();
         // Draw elements to screen
         draw(batch, font);
         // Check for inputs
+        // Movement
+        playerSprite.setX(MathUtils.clamp(playerSprite.getX(),
+                                            PLATFORM_AREA_X, PLATFORM_AREA_MAX_X));
+        playerSprite.setY(MathUtils.clamp(playerSprite.getY(),
+                                            PLATFORM_AREA_Y, PLATFORM_AREA_MAX_Y));
         if (Gdx.input.isTouched()) {
-            inputMovementTouch(delta);
+            inputMovementTouch();
         }
-        inputMovementKeys(delta);
+        inputMovementKeys();
+    }
+
+    private void checkTimers() {
+        float delta = Gdx.graphics.getDeltaTime();
+        abilityChargeTimer += delta;
+        enemySpawnTimer += delta;
+        itemSpawnTimer += delta;
+
+        final float abilityChargeTimerMax = 1f;
+        if (abilityChargeTimer >= abilityChargeTimerMax) {
+            playerSprite.increaseCurrentCharge();
+            abilityChargeTimer = 0;
+        }
+        final float enemySpawnTimerMax = 5.0f;
+        if (enemySpawnTimer >= enemySpawnTimerMax) {
+            createEnemy();
+            enemySpawnTimer = 0;
+        }
+        final float itemSpawnTimerMax = 2.5f;
+        if (itemSpawnTimer >= itemSpawnTimerMax) {
+            playerSprite.increaseCurrentCharge();
+            itemSpawnTimer = 0;
+        }
+    }
+
+    private void createEnemy() {
+        // create the drop sprite
+        Enemy enemySprite = Generate.createStandardZombie(standardZombieTexture);
+        enemySprite.setSize(SPRITE_WIDTH, SPRITE_HEIGHT);
+        // Randomize spawn location
+        enemySprite.setX(MathUtils.random(PLATFORM_AREA_X, PLATFORM_AREA_MAX_X));
+        enemySprite.setY(MathUtils.random(PLATFORM_AREA_Y, PLATFORM_AREA_MAX_Y));
+        enemySprites.add(enemySprite); // Add it to the list
     }
 
     private void draw(final SpriteBatch batch, final BitmapFont font) {
-        // Platform size
-        final float platformWidth = 9f * 150f;
-        final float platformHeight = 7f * 150f;
         // Draws elements to screen
         batch.begin();
             batch.draw(backgroundTexture, 0, 0, RotNRun.VIRTUAL_WIDTH, RotNRun.VIRTUAL_HEIGHT);
-            batch.draw(platformTexture, RotNRun.VIRTUAL_WIDTH / 2f - platformWidth / 2f,
-                0, platformWidth, platformHeight);
-            playerSprite.draw(batch);
+            batch.draw(platformTexture, RotNRun.VIRTUAL_WIDTH / 2f - PLATFORM_WIDTH / 2f,
+                0, PLATFORM_WIDTH, PLATFORM_HEIGHT);
             for (Item item : itemSprites) {
                 item.draw(batch);
             }
             for (Enemy enemy : enemySprites) {
                 enemy.draw(batch);
             }
+            playerSprite.draw(batch);
         batch.end();
     }
 
-    private void inputMovementTouch(final float delta) {
+    private void inputMovementTouch() {
+        float delta = Gdx.graphics.getDeltaTime();
         game.setMousePosition();
         if (playerSprite.getX() > RotNRun.MOUSE_POSITION.x) {
             playerSprite.translateX(-PLAYER_SPEED * delta);
@@ -135,7 +186,8 @@ public class GameScreen implements Screen {
         }
     }
 
-    private void inputMovementKeys(final float delta) {
+    private void inputMovementKeys() {
+        float delta = Gdx.graphics.getDeltaTime();
         game.setMousePosition();
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)
             || Gdx.input.isKeyPressed(Input.Keys.D)
