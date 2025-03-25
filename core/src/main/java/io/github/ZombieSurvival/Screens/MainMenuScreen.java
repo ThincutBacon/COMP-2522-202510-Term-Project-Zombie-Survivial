7dd80cf407c0603ffbcb17dc606ac6ade61abd62
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-25 11:50:13 -0700

    Create exit game button and modulate repetitive code

--- core/src/main/java/io/github/ZombieSurvival/Screens/MainMenuScreen.java
+++ core/src/main/java/io/github/ZombieSurvival/Screens/MainMenuScreen.java
@@ -15,17 +15,25 @@ import io.github.ZombieSurvival.RotNRun;
  * @version 2025
  */
 public class MainMenuScreen implements Screen {
+    // Current running game
     private final RotNRun game;
     // Start Button Values
     private final float startWidth = 900f;
     private final float startHeight = 90f;
     private final float startX = (RotNRun.VIRTUAL_WIDTH / 2f) - (startWidth / 2f);
-    private final float startY = (RotNRun.VIRTUAL_HEIGHT / 2f) - 90f;
+    private final float startY = (RotNRun.VIRTUAL_HEIGHT / 2f) - 60f;
     private final float startMaxX = startX + startWidth;
     private final float startMaxY = startY + startHeight;
+    private final float buttonLayoutOffsetY = -150f;
+    // Exit Button Values
+    private final float exitLength = 150f;
+    private final float exitXY = 100f;
+    private final float exitMaxXY = exitXY + exitLength;
     // Textures
     private final Texture startButtonInactive;
     private final Texture startButtonActive;
+    private final Texture exitButtonInactive;
+    private final Texture exitButtonActive;
 
     /**
      * Constructs a MainMenuScreen object with the specified instance of game.
@@ -41,6 +49,8 @@ public class MainMenuScreen implements Screen {
         //Textures
         startButtonInactive = new Texture("Start_Button_Inactive.png");
         startButtonActive = new Texture("Start_Button_Active.png");
+        exitButtonInactive = new Texture("Exit_Button_Inactive.png");
+        exitButtonActive = new Texture("Exit_Button_Active.png");
     }
 
     @Override
@@ -48,13 +58,15 @@ public class MainMenuScreen implements Screen {
 
     }
 
-    private float getPositionOffsetX(final float frameWidth, final BitmapFont bitmapFont, final String value) {
+    private float getPositionOffsetX(final float frameWidth, final BitmapFont bitmapFont,
+                                     final String value) {
         GlyphLayout glyphLayout = new GlyphLayout();
         glyphLayout.setText(bitmapFont, value);
         return (frameWidth / 2) - (glyphLayout.width / 2);
     }
 
-    private float getPositionOffsetY(final float frameHeight, final BitmapFont bitmapFont, final String value) {
+    private float getPositionOffsetY(final float frameHeight, final BitmapFont bitmapFont,
+                                     final String value) {
         GlyphLayout glyphLayout = new GlyphLayout();
         glyphLayout.setText(bitmapFont, value);
         return (frameHeight / 2) - (glyphLayout.height / 2);
@@ -74,14 +86,15 @@ public class MainMenuScreen implements Screen {
         SpriteBatch batch = game.getSpriteBatch();
         BitmapFont font = game.getFont();
         // Draw elements to screen
-        draw(batch, font);
+        batch.begin();
+            drawTitle(batch, font);
+            drawButtons(batch);
+        batch.end();
         // Check for input
         input();
-
-
     }
 
-    private void draw(SpriteBatch batch, BitmapFont font) {
+    private void drawTitle(final SpriteBatch batch, final BitmapFont font) {
         // Numbers
         float xCenter = getPositionOffsetX(RotNRun.VIRTUAL_WIDTH, font,
             "Welcome to Rot 'N' Run!!!");
@@ -92,30 +105,54 @@ public class MainMenuScreen implements Screen {
         final float welcomeTextY = yCenter + 300f;
         final float interactTextY = yCenter + 220f;
         // Draws elements to screen
-        batch.begin();
         font.draw(batch, "Welcome to Rot 'N' Run!!!", xCenter, welcomeTextY);
         font.draw(batch, "Click START to begin!", x2Center, interactTextY);
+    }
 
+    private void drawButtons(final SpriteBatch batch) {
         // Display button logic
         game.setMousePosition();
         // Start Button
-        if ((RotNRun.MOUSE_POSITION.x >= startX && RotNRun.MOUSE_POSITION.x <= startMaxX)
-            && (RotNRun.MOUSE_POSITION.y >= startY && RotNRun.MOUSE_POSITION.y <= startMaxY)) {
+        if (game.checkMouseOnButton(startX, startMaxX, startY, startMaxY)) {
             batch.draw(startButtonActive, startX, startY, startWidth, startHeight);
         } else {
             batch.draw(startButtonInactive, startX, startY, startWidth, startHeight);
         }
-        batch.end();
+        // Another Button
+        if (game.checkMouseOnButton(startX, startMaxX, startY
+            + buttonLayoutOffsetY, startMaxY + buttonLayoutOffsetY)) {
+            batch.draw(startButtonActive, startX, startY + buttonLayoutOffsetY,
+                startWidth, startHeight);
+        } else {
+            batch.draw(startButtonInactive, startX, startY + buttonLayoutOffsetY,
+                startWidth, startHeight);
+        }
+        // Exit Button
+        if (game.checkMouseOnButton(exitXY, exitMaxXY, exitXY, exitMaxXY)) {
+            batch.draw(exitButtonActive, exitXY, exitXY, exitLength, exitLength);
+        } else {
+            batch.draw(exitButtonInactive, exitXY, exitXY, exitLength, exitLength);
+        }
     }
 
     private void input() {
-        // Game start on click start
+        // On click
         if (Gdx.input.isTouched()) {
             game.setMousePosition();
-            if ((RotNRun.MOUSE_POSITION.x >= startX && RotNRun.MOUSE_POSITION.x <= startMaxX)
-                && (RotNRun.MOUSE_POSITION.y >= startY && RotNRun.MOUSE_POSITION.y <= startMaxY)) {
+            // Game start
+            if (game.checkMouseOnButton(startX, startMaxX, startY, startMaxY)) {
                 game.setScreen(new GameScreen(game));
                 dispose();
+            }
+            if (game.checkMouseOnButton(startX, startMaxX, startY
+                + buttonLayoutOffsetY, startMaxY + buttonLayoutOffsetY)) {
+                game.setScreen(new GameScreen(game));
+                dispose();
+            }
+            // Quit game
+            if (game.checkMouseOnButton(exitXY, exitMaxXY, exitXY, exitMaxXY)) {
+                dispose();
+                Gdx.app.exit();
             }
         }
     }
