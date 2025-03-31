7932be3bb9436b301c0c8cf3151118d9ab2279f9
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-03-31 16:40:08 -0700

    Create GameOverScreen class

---
+++ core/src/main/java/io/github/ZombieSurvival/Screens/GameOverScreen.java
@@ -0,0 +1,58 @@
+package io.github.ZombieSurvival.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import io.github.ZombieSurvival.RotNRun;
+
+public class GameOverScreen implements Screen {
+    // Current running game
+    private final RotNRun game;
+
+    /**
+     * Constructs a GameOverScreen object with the specified instance of game.
+     *
+     * @param game a RotNRun
+     * @throws IllegalArgumentException if game is null
+     */
+    public GameOverScreen(final RotNRun game) {
+        if (game == null) {
+            throw new IllegalArgumentException("There is no game to the apply screen to.");
+        }
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
