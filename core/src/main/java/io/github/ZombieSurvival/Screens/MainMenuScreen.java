8112254995d48cfee7e270b1987dfc3c8df5be70
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-04-02 12:54:09 -0700

    Implement unlock-able difficulties

--- core/src/main/java/io/github/ZombieSurvival/Screens/MainMenuScreen.java
+++ core/src/main/java/io/github/ZombieSurvival/Screens/MainMenuScreen.java
@@ -1,6 +1,7 @@
 package io.github.ZombieSurvival.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -21,21 +22,16 @@ import java.util.Scanner;
 public class MainMenuScreen implements Screen {
     // Current running game
     private final RotNRun game;
-    // Start Button Values
-    private final float startWidth = 900f;
-    private final float startHeight = 90f;
-    private final float startX = (RotNRun.VIRTUAL_WIDTH / 2f) - (startWidth / 2f);
-    private final float startY = 520f;
-    private final float startMaxX = startX + startWidth;
-    private final float startMaxY = startY + startHeight;
-    private final float buttonLayoutOffsetY = -200f;
     // Difficulty Button Values
+    private final float panelWidth = 900f;
     private final float difficultyWidth = 290f;
-    private final float difficultyHeight = 100f;
+    private final float difficultyHeight = 120f;
     private final float difficultyPadding = difficultyWidth + 15f;
-    private final float difficultyY = 430f;
+
+    private final float difficultyY = 380f;
     private final float difficultyMaxY = difficultyY + difficultyHeight;
-    private final float difficultyEasyX = (RotNRun.VIRTUAL_WIDTH / 2f) - (startWidth / 2f);
+
+    private final float difficultyEasyX = (RotNRun.VIRTUAL_WIDTH / 2f) - (panelWidth / 2f);
     private final float difficultyEasyMaxX = difficultyEasyX + difficultyWidth;
     private final float difficultyNormalX = difficultyEasyX + difficultyPadding;
     private final float difficultyNormalMaxX = difficultyNormalX + difficultyWidth;
@@ -47,20 +43,25 @@ public class MainMenuScreen implements Screen {
     private final float exitMaxXY = exitXY + exitLength;
     // Textures
     private final Texture gameTitle;
-    private final Texture startButtonInactive;
-    private final Texture startButtonActive;
     private final Texture easyButtonInactive;
     private final Texture easyButtonActive;
     private final Texture normalButtonInactive;
     private final Texture normalButtonActive;
+    private final Texture normalButtonLocked;
     private final Texture hardButtonInactive;
     private final Texture hardButtonActive;
+    private final Texture hardButtonLocked;
     private final Texture exitButtonInactive;
     private final Texture exitButtonActive;
-
+    // Saved values
     private int easyHighScore = 0;
+    private boolean easyNewHighScore = false;
     private int normalHighScore = 0;
+    private boolean normalNewHighScore = false;
     private int hardHighScore = 0;
+    private boolean hardNewHighScore = false;
+    private boolean normalUnlocked = false;
+    private boolean hardUnlocked = false;
 
     /**
      * Constructs a MainMenuScreen object with the specified instance of game.
@@ -75,23 +76,26 @@ public class MainMenuScreen implements Screen {
         this.game = game;
         //Textures
         gameTitle = new Texture("Game_Title.png");
-        startButtonInactive = new Texture("Start_Button_Inactive.png");
-        startButtonActive = new Texture("Start_Button_Active.png");
         easyButtonInactive = new Texture("Difficulty_Easy_Inactive.png");
         easyButtonActive = new Texture("Difficulty_Easy_Active.png");
         normalButtonInactive = new Texture("Difficulty_Normal_Inactive.png");
         normalButtonActive = new Texture("Difficulty_Normal_Active.png");
+        normalButtonLocked = new Texture("Difficulty_Normal_Locked.png");
         hardButtonInactive = new Texture("Difficulty_Hard_Inactive.png");
         hardButtonActive = new Texture("Difficulty_Hard_Active.png");
+        hardButtonLocked = new Texture("Difficulty_Hard_Locked.png");
         exitButtonInactive = new Texture("Exit_Button_Inactive.png");
         exitButtonActive = new Texture("Exit_Button_Active.png");
-
-
         try {
             Scanner scanner = new Scanner(RotNRun.SAVE_FILE_PATH);
             easyHighScore = scanner.nextInt();
+            easyNewHighScore = scanner.nextBoolean();
             normalHighScore = scanner.nextInt();
+            normalNewHighScore = scanner.nextBoolean();
             hardHighScore = scanner.nextInt();
+            hardNewHighScore = scanner.nextBoolean();
+            normalUnlocked = scanner.nextBoolean();
+            hardUnlocked = scanner.nextBoolean();
             System.out.println("Easy Difficulty High Score: " + easyHighScore);
             System.out.println("Normal Difficulty High Score: " + normalHighScore);
             System.out.println("Hard Difficulty High Score: " + hardHighScore);
@@ -99,7 +103,6 @@ public class MainMenuScreen implements Screen {
         } catch (IOException error) {
             System.out.println("Failed to read scores from save file.");
         }
-
     }
 
     @Override
@@ -128,6 +131,7 @@ public class MainMenuScreen implements Screen {
         batch.end();
         // Check for input
         input();
+        inputCheats();
     }
 
     /*
@@ -138,7 +142,7 @@ public class MainMenuScreen implements Screen {
         final float titleWidth = 1200f;
         final float titleHeight = 600f;
         final float titleX = (RotNRun.VIRTUAL_WIDTH / 2f) - (titleWidth / 2f);
-        final float titleY = startY + 200f;
+        final float titleY = 650f;
         batch.draw(gameTitle, titleX, titleY, titleWidth, titleHeight);
     }
 
@@ -148,21 +152,6 @@ public class MainMenuScreen implements Screen {
     private void drawButtons(final SpriteBatch batch) {
         // Display button logic
         game.setMousePosition();
-//        // Start Button
-//        if (game.checkMouseOnButton(startX, startMaxX, startY, startMaxY)) {
-//            batch.draw(startButtonActive, startX, startY, startWidth, startHeight);
-//        } else {
-//            batch.draw(startButtonInactive, startX, startY, startWidth, startHeight);
-//        }
-        // Another Button
-        if (game.checkMouseOnButton(startX, startMaxX, startY
-            + buttonLayoutOffsetY, startMaxY + buttonLayoutOffsetY)) {
-            batch.draw(startButtonActive, startX, startY + buttonLayoutOffsetY,
-                startWidth, startHeight);
-        } else {
-            batch.draw(startButtonInactive, startX, startY + buttonLayoutOffsetY,
-                startWidth, startHeight);
-        }
         // Exit Button
         if (game.checkMouseOnButton(exitXY, exitMaxXY, exitXY, exitMaxXY)) {
             batch.draw(exitButtonActive, exitXY, exitXY, exitLength, exitLength);
@@ -175,66 +164,101 @@ public class MainMenuScreen implements Screen {
      * Draws the difficulty high scores to screen.
      */
     private void drawDifficultyHighScores(final SpriteBatch batch) {
-        BitmapFont font = game.getFont();
+        BitmapFont normalText = game.getNormalText();
         final float padding = 20f;
 
         GlyphLayout glyphLayout = new GlyphLayout();
-        glyphLayout.setText(font, "HIGH SCORES");
-        float highScoreTextXCenter = (startWidth / 2) - (glyphLayout.width / 2);
+        glyphLayout.setText(normalText, "HIGH SCORES");
+        float highScoreTextXCenter = (panelWidth / 2) - (glyphLayout.width / 2);
         float highScoreTextY = difficultyMaxY + (glyphLayout.height + padding) * 2;
-        font.draw(batch, "HIGH SCORES",
+        normalText.draw(batch, "HIGH SCORES",
             difficultyEasyX + highScoreTextXCenter, highScoreTextY);
 
-        glyphLayout.setText(font, "000");
+        glyphLayout.setText(normalText, "000");
         float difficultyTextXCenter = (difficultyWidth / 2) - (glyphLayout.width / 2);
         float difficultyTextY = difficultyMaxY + glyphLayout.height + padding;
 
         // Easy
         String easyHS = String.format("%03d", easyHighScore);
-        font.draw(batch, easyHS,
+        normalText.draw(batch, easyHS,
             difficultyEasyX + difficultyTextXCenter, difficultyTextY);
 
         // Normal
         String normalHS = String.format("%03d", normalHighScore);
-        font.draw(batch, easyHS,
+        normalText.draw(batch, normalHS,
             difficultyNormalX + difficultyTextXCenter, difficultyTextY);
 
         // Hard
         String hardHS = String.format("%03d", hardHighScore);
-        font.draw(batch, hardHS,
+        normalText.draw(batch, hardHS,
             difficultyHardX + difficultyTextXCenter, difficultyTextY);
+        // Draw "new" text
+        if (easyNewHighScore || normalNewHighScore || hardNewHighScore) {
+            drawDifficultyNew(batch, difficultyTextXCenter, difficultyTextY);
+        }
     }
 
     /*
-     * Draws the difficulty button textures to screen.
+     * Draws the "new" text above the high scores.
+     */
+    private void drawDifficultyNew(final SpriteBatch batch,
+                                   final float difficultyTextXCenter,
+                                   final float difficultyTextY) {
+        BitmapFont yellowText = game.getYellowText();
+        final float offset = 25f;
+        // If high score was updated
+        if (easyNewHighScore) {
+            yellowText.draw(batch, "NEW",
+                difficultyEasyX + difficultyTextXCenter - offset, difficultyTextY + offset);
+        }
+        if (normalNewHighScore) {
+            yellowText.draw(batch, "NEW",
+                difficultyNormalX + difficultyTextXCenter - offset, difficultyTextY + offset);
+        }
+        if (hardNewHighScore) {
+            yellowText.draw(batch, "NEW",
+                difficultyHardX + difficultyTextXCenter - offset, difficultyTextY + offset);
+        }
+    }
+
+    /*
+     * Draws the difficulty selection buttons to screen.
      */
     private void drawDifficultyButtons(final SpriteBatch batch) {
         game.setMousePosition();
         // EASY
-        if (game.checkMouseOnButton(difficultyEasyX, difficultyEasyMaxX,
-            difficultyY, difficultyMaxY)) {
-            batch.draw(easyButtonActive, difficultyEasyX, difficultyY,
-                difficultyWidth, difficultyHeight);
-        } else {
-            batch.draw(easyButtonInactive, difficultyEasyX, difficultyY,
-                difficultyWidth, difficultyHeight);
-        }
+        drawDifficultyButton(batch, difficultyEasyX, difficultyEasyMaxX,
+            easyButtonActive, easyButtonInactive);
         // NORMAL
-        if (game.checkMouseOnButton(difficultyNormalX, difficultyNormalMaxX,
-            difficultyY, difficultyMaxY)) {
-            batch.draw(normalButtonActive, difficultyNormalX, difficultyY,
-                difficultyWidth, difficultyHeight);
+        if (normalUnlocked) {
+            drawDifficultyButton(batch, difficultyNormalX, difficultyNormalMaxX,
+                normalButtonActive, normalButtonInactive);
         } else {
-            batch.draw(normalButtonInactive, difficultyNormalX, difficultyY,
+            batch.draw(normalButtonLocked, difficultyNormalX, difficultyY,
                 difficultyWidth, difficultyHeight);
         }
         // HARD
-        if (game.checkMouseOnButton(difficultyHardX, difficultyHardMaxX,
+        if (hardUnlocked) {
+            drawDifficultyButton(batch, difficultyHardX, difficultyHardMaxX,
+                hardButtonActive, hardButtonInactive);
+        } else {
+            batch.draw(hardButtonLocked, difficultyHardX, difficultyY,
+                difficultyWidth, difficultyHeight);
+        }
+    }
+
+    /*
+     * Draws the button texture to screen.
+     */
+    private void drawDifficultyButton(final SpriteBatch batch,
+                                      final float xPosition, final float maxXPosition,
+                                      final Texture textureActive, final Texture textureInactive) {
+        if (game.checkMouseOnButton(xPosition, maxXPosition,
             difficultyY, difficultyMaxY)) {
-            batch.draw(hardButtonActive, difficultyHardX, difficultyY,
+            batch.draw(textureActive, xPosition, difficultyY,
                 difficultyWidth, difficultyHeight);
         } else {
-            batch.draw(hardButtonInactive, difficultyHardX, difficultyY,
+            batch.draw(textureInactive, xPosition, difficultyY,
                 difficultyWidth, difficultyHeight);
         }
     }
@@ -253,21 +277,20 @@ public class MainMenuScreen implements Screen {
                 dispose();
             }
             // Game select NORMAL
-            if (game.checkMouseOnButton(difficultyNormalX, difficultyNormalMaxX,
-                difficultyY, difficultyMaxY)) {
-                game.setScreen(new GameScreen(game, Difficulty.NORMAL));
-                dispose();
+            if (normalUnlocked) {
+                if (game.checkMouseOnButton(difficultyNormalX, difficultyNormalMaxX,
+                    difficultyY, difficultyMaxY)) {
+                    game.setScreen(new GameScreen(game, Difficulty.NORMAL));
+                    dispose();
+                }
             }
             // Game select HARD
-            if (game.checkMouseOnButton(difficultyHardX, difficultyHardMaxX,
-                difficultyY, difficultyMaxY)) {
-                game.setScreen(new GameScreen(game, Difficulty.HARD));
-                dispose();
-            }
-            // How to play button (NOT YET IMPLEMENTED)
-            if (game.checkMouseOnButton(startX, startMaxX, startY
-                + buttonLayoutOffsetY, startMaxY + buttonLayoutOffsetY)) {
-                dispose();
+            if (hardUnlocked) {
+                if (game.checkMouseOnButton(difficultyHardX, difficultyHardMaxX,
+                    difficultyY, difficultyMaxY)) {
+                    game.setScreen(new GameScreen(game, Difficulty.HARD));
+                    dispose();
+                }
             }
             // Quit game
             if (game.checkMouseOnButton(exitXY, exitMaxXY, exitXY, exitMaxXY)) {
@@ -276,6 +299,18 @@ public class MainMenuScreen implements Screen {
             }
         }
     }
+
+    /*
+     * Checks for user inputs that unlock cheat functions.
+     */
+    private void inputCheats() {
+        // Unlock all difficulty options
+        if (Gdx.input.isKeyPressed(Input.Keys.U)) {
+            normalUnlocked = true;
+            hardUnlocked = true;
+        }
+    }
+
 
     /**
      * Updates viewport by width and height when window is resized.
