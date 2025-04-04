b881f8d495b81f720c610b5ff5c17d477bb9f159
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-04-04 11:56:10 -0700

    Add Java Docs

--- core/src/main/java/io/github/ZombieSurvival/Sprites/Difficulty.java
+++ core/src/main/java/io/github/ZombieSurvival/Sprites/Difficulty.java
@@ -1,5 +1,11 @@
 package io.github.ZombieSurvival.Sprites;
 
+/**
+ * Gameplay difficulty options.
+ *
+ * @author Kanon Nishiyama
+ * @version 2025
+ */
 public enum Difficulty {
     EASY,
     NORMAL,
