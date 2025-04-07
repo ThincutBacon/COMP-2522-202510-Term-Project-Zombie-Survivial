3722eb444af5ddb3bd8fab45eb255837bf765944
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-04-06 23:59:52 -0700

    Remove unnecessary code

--- core/src/main/java/io/github/ZombieSurvival/Screens/MainMenuScreen.java
+++ core/src/main/java/io/github/ZombieSurvival/Screens/MainMenuScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -369,6 +368,5 @@ public class MainMenuScreen implements Screen {
         exitButtonInactive.dispose();
         exitButtonActive.dispose();
         bgm.dispose();
-        select.dispose();
     }
 }
